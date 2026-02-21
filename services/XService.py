d800d07fc0553f0eb0b722621e97dc3d215076ae
Lueyo <84575656+lueyo@users.noreply.github.com>
2026-02-21 12:38:53 +0100

    xservice va

--- services/XService.py
+++ services/XService.py
@@ -18,14 +18,8 @@ class DownloadError(Exception):
 class XService:
     FX_TWITTER_URL = "https://fxtwitter.com/i/status/{x_id}"
     HEADERS = {
-        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:147.0) Gecko/20100101 Firefox/147.0",
+        "User-Agent": "curl/8.5.0",
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-        "Accept-Language": "es-ES,es;q=0.9,en-US;q=0.8,en;q=0.7",
-        "Accept-Encoding": "gzip, deflate, br, zstd",
-        "DNT": "1",
-        "Sec-GPC": "1",
-        "Connection": "keep-alive",
-        "Upgrade-Insecure-Requests": "1",
     }
 
     @classmethod
